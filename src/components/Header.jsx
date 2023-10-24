2e07b9ce9a878a7cf3082afa571aaccc0abe7ab8
saipranay47 <saipranayadepu07@gmail.com>
2023-10-24 16:17:13 +0530

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -110,38 +110,10 @@ export default function Header() {
                     >
                       <Popover.Panel className="hidden md:block absolute z-10 top-full inset-x-0 transform bg-white">
                         <div className="max-w-7xl mx-auto grid gap-y-6 px-4 py-6 sm:grid-cols-2 sm:gap-8 sm:px-6 sm:py-8 lg:grid-cols-4 lg:px-8 lg:py-12 xl:py-16">
-                          {solutions.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-gray-50"
-                            >
-                              <div className="flex md:h-full lg:flex-col">
-                                <div className="flex-shrink-0">
-                                  <span className="inline-flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                    <item.icon
-                                      className="h-6 w-6"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                </div>
-                                <div className="ml-4 md:flex-1 md:flex md:flex-col md:justify-between lg:ml-0 lg:mt-4">
-                                  <div>
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                      {item.description}
-                                    </p>
-                                  </div>
-                                  <p className="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">
-                                    Learn more{" "}
-                                    <span aria-hidden="true">&rarr;</span>
-                                  </p>
-                                </div>
-                              </div>
-                            </a>
-                          ))}
+                          {/* <div
+                            className="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-gray-50"
+                          >
+                          </div> */}
                         </div>
                       </Popover.Panel>
                     </Transition>
